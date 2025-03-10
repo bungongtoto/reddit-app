03d5b289ef01baff1c48093e3a52cb7ea90853ac
bungongtoto <bungongkingsley@gmail.com>
2025-03-10 10:42:01 +0000

    category filter

---
+++ src/features/categoryFilter/CategoryFilter.js
@@ -0,0 +1,18 @@
+import "./CategoryFilter.css";
+
+function CategoryFilter() {
+  return (
+    <div className="category-filter">
+      <button>Arts</button>
+      <button>Anime</button>
+      <button>Cinema</button>
+      <button>Movies</button>
+      <button>Arts</button>
+      <button>Anime</button>
+      <button>Cinema</button>
+      <button>Movies</button>
+    </div>
+  );
+}
+
+export default CategoryFilter;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
+import CategoryFilter from "../features/categoryFilter/CategoryFilter.js";
 import Search from "../features/search/Search.js";
 
 function HomePage(){
     return (
         <div className="homePage">
             <Search />
+            <CategoryFilter />
         </div>
     )
 }
